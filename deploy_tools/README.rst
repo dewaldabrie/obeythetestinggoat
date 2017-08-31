4cc4291548dade6c77211b575a35ad3bc3c9746d
dabrie <dabrie@cmcrc.com>
2017-08-31 16:48:35 +1000

    Upgrade provisioning script and add README for deploy tools

---
+++ deploy_tools/README.rst
@@ -0,0 +1,9 @@
+DEPLOY TOOLS
+============
+
+
+Usage
+`````
+Copy to server like so::
+
+    $ scp -r -i ~/.ssh/MyAmznKeyPair.pem deploy_tools dewald@52.25.219.198:/home/dewald
--- deploy_tools/provision.sh
+++ deploy_tools/provision.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
 url=$1
+
+# Install Python3.6, venv, and nginx
+sudo add-apt-repository -y ppa:fkrull/deadsnakes
+sudo apt-get install -y nginx git python3.6 python3.6-venv
+
+# Use nginx template to configure server to serve our site
+
 # We substitute the string SITENAME for the address of our site,
 # with the s/replaceme/withthis/g syntax.
 # We pipe (|) the output of that to a root-user process (sudo),
@@ -14,10 +21,16 @@ sed "s/SITENAME/$url/g" \
 sudo ln -s ../sites-available/$url \
     /etc/nginx/sites-enabled/$url
 
+# Test nginx config
+sudo nginx -t
+
 # And we write the Systemd service, with another sed:
 sed "s/SITENAME/$url/g" \
     gunicorn-systemd.template.service \
     | sudo tee /etc/systemd/system/gunicorn-$url.service
+
+# Check validity of service configuration
+systemd-analyze verify /etc/systemd/system/gunicorn-$url.service
 
 # Finally we start both services:
 sudo systemctl daemon-reload
